a0d8857392b1eb719177383c6e2ea65f2e256aee
Takayoshi Kochi <kochi@chromium.org>
2014-06-24 16:29:23 +0900

    Translate inline style.

--- inputmode.js
+++ inputmode.js
@@ -5,16 +5,45 @@
 
 var inputMode = {
   searchStyleSheets: function() {
+    var count = 0;
+
     var len = document.styleSheets.length;
     for (var i = 0; i < len; ++i) {
       var css = document.styleSheets[i];
       for (var i = 0; i < css.cssRules.length; ++i) {
-	var r = css.cssRules[i];
-	if (r == 1) {  // style rule
-	  console.log(r.cssText);
-	}
+        var r = css.cssRules[i];
+        if (r.type == 1) {  // style rule
+          // FIXME:
+          // It seems unsupported properties are discarded at parsing.
+          console.log(r.cssText);
+        }
       }
     }
+
+    // auto -> ?
+    // active -> inputmode=kana
+    // inactive -> inputmode=latin
+    // disabled -> inputmode=latin (not 100%, but good enough)
+    var els = document.querySelectorAll('input[style]');
+    for (var i = 0; i < els.length; ++i) {
+      if (!els[i].hasAttribute('style'))
+        continue;
+      var style = els[i].getAttribute('style');
+      var styles = style.split(/;\s*/);
+      for (var j = 0; j < styles.length; ++j) {
+        if (styles[j].match(/ime-mode/)) {
+          var property = styles[j].substr(styles[j].indexOf(':') + 1);
+          if (property.match(/inactive/) || property.match(/disabled/)) {
+            els[i].setAttribute('inputmode', 'latin');
+	    ++count;
+          } else if (property.match(/active/)) {
+            els[i].setAttribute('inputmode', 'kana');
+	    ++count;
+          }
+        }
+      }
+    }
+    console.log('replaced ' + count + ' styles.');
   },
 
   init: function() {
